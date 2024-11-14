3119b138b0ee7b73dd6d2eeb8a58fd065e31178c
GedSargent <120935232+GedSargent@users.noreply.github.com>
2024-11-14 14:04:35 +0000

    Added three buttons to home page

--- app/root.tsx
+++ app/root.tsx
@@ -16,12 +16,23 @@ export default function App() {
 		<Document nonce={nonce}>
 			<div className="flex h-screen flex-col justify-between">
 				<div className="flex-1">
-					<main className="grid h-full place-items-center">
+					<main className="container grid h-full place-items-center">
 						<h1 className="text-mega">Your Journey Begins!</h1>
 						<p className="text-base text-gray-600 md:text-lg lg:text-2xl">
 							Welcome to Epic News, where the latest developments in tech are
 							found.
 						</p>
+						<div className="flex gap-8">
+							<button className="bg-blue-700 px-8 py-4 hover:bg-blue-800">
+								Click me
+							</button>
+							<button className="bg-blue-700 px-8 py-4 hover:bg-blue-800">
+								Click me
+							</button>
+							<button className="bg-blue-700 px-8 py-4 hover:bg-blue-800">
+								Click me
+							</button>
+						</div>
 					</main>
 				</div>
 			</div>
