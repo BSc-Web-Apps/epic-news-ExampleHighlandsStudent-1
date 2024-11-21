c9c1619fefa0db639e7c8c204fd55f53f73e1755
GedSargent <120935232+GedSargent@users.noreply.github.com>
2024-11-21 16:00:43 +0000

    Finished HeroCallToAction lesson

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,5 @@
 import { type LinksFunction } from '@remix-run/node'
-import { useLoaderData } from '@remix-run/react'
+import { Link, useLoaderData } from '@remix-run/react'
 import Document from '~/components/shared-layout/Document'
 import ThemeSwitch from '~/components/shared-layout/ThemeSwitch'
 import { useNonce } from '~/utils/nonce-provider.ts'
@@ -8,6 +8,9 @@ import { type loader } from './__root.server'
 import FooterMenuRight from './components/organisms/Footer/FooterMenuRight'
 import HeaderWithSearch from './components/organisms/HeaderWithSearch'
 import useTheme from './hooks/useTheme.tsx'
+import HeroCallToAction from './components/organisms/Hero/HeroCallToAction.tsx'
+import heroImage from '~/assets/jpg/sample-hero.jpg'
+import { Button } from './components/atoms/Button.tsx'
 
 export const links: LinksFunction = () => {
 	return rootLinkElements
@@ -26,9 +29,27 @@ export default function App() {
 				<HeaderWithSearch />
 
 				<div className="flex-1">
-					<main className="grid h-full place-items-center">
-						<h1 className="text-mega">Welcome to Epic News!</h1>
+					<main>
+						<HeroCallToAction
+							image={heroImage}
+							imageRight={true}
+							hasBackgroundColour={true}
+						>
+							<div className="flex h-full flex-1 flex-col justify-between p-16">
+								<div className="flex flex-col gap-8">
+									<h2 className="text-h2">Welcome to Epic News</h2>
+									<p className="text-lg">
+										Keep up to date with the latest tech news.
+									</p>
+								</div>
+								<Button asChild variant="default" size="lg">
+									<Link to="/signup">Sign up</Link>
+								</Button>
+							</div>
+						</HeroCallToAction>
 					</main>
+
+					<div className="w-full py-16"></div>
 				</div>
 
 				<div className="container flex justify-between pb-5">
